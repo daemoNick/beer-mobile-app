5e5d9f4d467e9938d3b87bce8caa4e1b1b81a1db
nickoloaded <essiendominic1@outlook.com>
2020-04-05 23:56:10 +0100

    Added logic for new beer, reactive form controls and rating

--- src/app/models/BeerItem.ts
+++ src/app/models/BeerItem.ts
@@ -9,3 +9,13 @@ export interface BeerItem {
     flavour: string[];
     description: string;
 }
+
+export interface PostBeerItem{
+    beer_name: string;
+    brewer: string;
+    price: number;
+    serving_type: string;
+    rating: number;
+    flavour: string[];
+    description: string;
+}
--- src/app/tabs/add-new/add-new.page.ts
+++ src/app/tabs/add-new/add-new.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Flavours } from 'src/app/mock-data/data';
+import { PostBeerItem } from 'src/app/models/BeerItem';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { StarRatingComponent } from 'ng-starrating';
+
 
 @Component({
   selector: 'app-add-new',
@@ -10,9 +14,44 @@ export class AddNewPage implements OnInit {
 
   flavours = Flavours;
 
+  postData: PostBeerItem = {
+    beer_name: '',
+    brewer: '',
+    price: 0,
+    serving_type: '',
+    rating: 0,
+    flavour: [],
+    description: ''
+  };
+
+  newBeerForm = new FormGroup({
+    beer_name: new FormControl('', [Validators.required]),
+    brewer: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    serving_type: new FormControl('', [Validators.required]),
+    rating: new FormControl('', [Validators.required]),
+    flavour: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRate($event: {oldValue: number, newValue: number, starRating: StarRatingComponent}) {
+    console.log(`Old Value:${$event.oldValue},
+      New Value: ${$event.newValue},
+      Checked Color: ${$event.starRating.checkedcolor},
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+
+    this.newBeerForm.controls.rating.setValue($event.newValue);
+    console.log('This rating value is ' + this.newBeerForm.controls.rating.value);
+
+  }
+
+  submitAction(){
+    console.log(this.newBeerForm);
   }
 
 }
